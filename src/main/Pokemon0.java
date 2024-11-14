f6ec3cae6f9476b931b68fc9401150365ca15a90
Nathan <Nathan@natepc2.private.univ-tlse3.fr>
2024-11-14 15:18:49 +0100

    first push

---
+++ src/main/Pokemon0.java
@@ -0,0 +1,46 @@
+package main;
+
+import java.util.Random;
+
+public class Pokemon0 {
+	//ATTRIBUTS
+	private String nom;
+	private int niveau;
+	private int hp;
+	private double atk;
+	
+	//CONSTRUCTEUR
+	public Pokemon0(String nom) {
+		this.nom = nom;
+		Random random = new Random();
+		this.niveau = random.nextInt(10);
+		this.hp = this.niveau * 2;
+		this.atk = (this.niveau / 2) + 1;
+	}
+	
+	//GETTERS
+	public String getName() {
+		return nom;
+	}
+
+	public int getNiveau() {
+		return niveau;
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public double getAtk() {
+		return atk;
+	}
+	
+	//METHODES
+	public boolean isKO() {
+		return hp == 0;
+	}
+	
+	
+	
+	
+}
